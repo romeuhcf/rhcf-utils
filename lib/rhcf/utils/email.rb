8416c01add16bf899ca20c462e0ae21f2994f771
Romeu Fonseca <romeu.hcf@gmail.com>
2014-05-12 15:44:18 -0300

    Bind interface is now optional

--- lib/rhcf/utils/email.rb
+++ lib/rhcf/utils/email.rb
@@ -27,24 +27,24 @@ end
 module Rhcf
   module Utils
     module Email
-      def self.get_mxs(domain) 
+      def self.get_mxs(domain)
         Resolv::DNS.open do |dns|
           dns.getresources(domain, Resolv::DNS::Resource::IN::MX).collect{|x| x.exchange.to_s}
         end
       end
-  
-      def self.transmit(from, to, body, bind_interface, ehlo = 'localhost.localdomain')
+
+      def self.transmit(from, to, body, bind_interface=nil, ehlo = 'localhost.localdomain')
         domain = to.split('@').last
-      
-        chat = StringIO.new 
+
+        chat = StringIO.new
         mx = get_mxs(domain).sample
         smtp = Net::SMTP.new(mx, 25)
-        smtp.bind_at bind_interface 
+        smtp.bind_at bind_interface if bind_interface
         result = smtp.start(ehlo) do |smtp|
           smtp.socket.debug_output = chat
           smtp.send_message body, from, to
         end
-   
+
         {status: result.status, string: result.string, chat: chat.string}
       end
     end
